3632a7224f7e54ad67c6e2ba18b82ab64b2c1b48
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-08-17 13:18:47 +0300

    Add project files.

---
+++ IdentityMessagingApplication.PresentationLayer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IdentityMessagingApplication.PresentationLayer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
