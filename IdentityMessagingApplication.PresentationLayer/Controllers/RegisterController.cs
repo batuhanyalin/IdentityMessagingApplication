2d97b3d93de82ed3c20bbf723eb5a67be80941f2
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-08-17 13:31:37 +0300

    Katman klasörleri oluşturuldu.

---
+++ IdentityMessagingApplication.PresentationLayer/Controllers/RegisterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityMessagingApplication.PresentationLayer.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
