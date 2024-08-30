a40365598edb0a04fa862abd51c3562c91150c30
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-08-30 19:45:35 +0300

    ReadMessageDto ya Receiver eklendi ve ReadMessage viewine if sorgusu içinde şarta bağlı olarak kimden ve kime yapısı kuruldu.

--- IdentityMessagingApplication.DtoLayer/MessageDtos/ReadMessageDto.cs
+++ IdentityMessagingApplication.DtoLayer/MessageDtos/ReadMessageDto.cs
@@ -12,6 +12,8 @@ namespace IdentityMessagingApplication.DtoLayer.MessageDtos
         public int MessageId { get; set; }
         public int SenderId { get; set; }
         public AppUser Sender { get; set; }
+        public int ReceiverId { get; set; }
+        public AppUser Receiver { get; set; }
         public string Subject { get; set; }
         public string Content { get; set; }
         public DateTime SendingTime { get; set; }
