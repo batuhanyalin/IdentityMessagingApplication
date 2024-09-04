153c5530f9e67fbc2f6747c8c109eb006b21e1a1
Batuhan YALIN <batuhanyalin2@gmail.com>
2024-09-04 19:13:02 +0300

    Role Controller oluşturuldu, profileController kaldırıldı. Rol için gerekli Dto sınıfları oluşturuldu. Mapping işlemleri yapıldı. Bütün CRUD fonksiyonlarına cevap veren AJAX ve sweetalert içeren kodlar oluşturuldu ve sistem çalışır hale getirildi.

--- IdentityMessagingApplication.PresentationLayer/Areas/Admin/Controllers/RoleController.cs
+++ IdentityMessagingApplication.PresentationLayer/Areas/Admin/Controllers/RoleController.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using IdentityMessagingApplication.BusinessLayer.Abstract;
+using IdentityMessagingApplication.DtoLayer.RoleDtos;
+using IdentityMessagingApplication.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json.Nodes;
 
 namespace IdentityMessagingApplication.PresentationLayer.Areas.Admin.Controllers
 {
@@ -6,9 +13,78 @@ namespace IdentityMessagingApplication.PresentationLayer.Areas.Admin.Controllers
     [Route("Admin/[controller]")]
     public class RoleController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<AppRole> _roleManager;
+        private readonly IMapper _mapper;
+
+        public RoleController(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager, IMapper mapper)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _mapper = mapper;
+        }
+
+        [Route("Index")]
         public IActionResult Index()
         {
-            return View();
+            var values = _roleManager.Roles.ToList();
+            var roles = _mapper.Map<List<ListRoleDto>>(values);
+            return View(roles);
+        }
+        [Route("JSUpdateRole")]
+        public async Task<IActionResult> JSUpdateRole(UpdateRoleDto updateRoleDto)
+        {
+            var value = _roleManager.Roles.FirstOrDefault(x => x.Id == updateRoleDto.Id);
+            if (value == null)
+            {
+                return Json(new { success = false, messsage = "Rol bulunamadı." });
+            }
+            value.Name = updateRoleDto.Name;
+            var result = await _roleManager.UpdateAsync(value);
+            if (result.Succeeded)
+            {
+                return Json(new { success = true, message = "Rol başarıyla güncellendi!" });
+            }
+            else
+            {
+                return Json(new { success = false, message = "Güncelleme sırasında bir hata oluştu." });
+            }
+        }
+        [Route("JSCreateRole")]
+        public async Task<IActionResult> JSCreateRole(CreateRoleDto createRoleDto)
+        {
+            if (createRoleDto.Name == null)
+            {
+                return Json(new { success = false, message = "Rol adı boş geçilemez!" });
+            }
+            AppRole appRole = new AppRole()
+            {
+                Name = createRoleDto.Name,
+            };
+            var result = await _roleManager.CreateAsync(appRole);
+            if (result.Succeeded)
+            {
+                return Json(new { success = true, message = "Rol başarıyla kaydedildi." });
+            }
+            else
+            {
+                return Json(new { success = false, message = "Rol kaydetme işlemi sırasında bir hata oluştu" });
+            }
+        }
+        [Route("GetRole/{id:int}")]
+        public ActionResult GetRole(int id)
+        {
+            var value = _roleManager.Roles.FirstOrDefault(x => x.Id == id);
+            var role = _mapper.Map<ListRoleDto>(value);
+            var jsonRole = JsonConvert.SerializeObject(role);
+            return Json(jsonRole);
+        }
+        [Route("DeleteRole/{id:int}")]
+        public IActionResult DeleteRole(int id)
+        {
+            var value = _roleManager.Roles.FirstOrDefault(x => x.Id == id);
+            _roleManager.DeleteAsync(value);
+            return Json(new { success = true });
         }
     }
 }
--- IdentityMessagingApplication.PresentationLayer/Mapping/GeneralMapping.cs
+++ IdentityMessagingApplication.PresentationLayer/Mapping/GeneralMapping.cs
@@ -2,6 +2,7 @@
 using IdentityMessagingApplication.DtoLayer.LoginDtos;
 using IdentityMessagingApplication.DtoLayer.MessageDtos;
 using IdentityMessagingApplication.DtoLayer.RegisterDtos;
+using IdentityMessagingApplication.DtoLayer.RoleDtos;
 using IdentityMessagingApplication.DtoLayer.UserDtos;
 using IdentityMessagingApplication.EntityLayer.Concrete;
 
@@ -24,6 +25,11 @@ namespace IdentityMessagingApplication.PresentationLayer.Mapping
             CreateMap<AppUser, LoginDto>().ReverseMap();
             CreateMap<AppUser, RegisterDto>().ReverseMap();
             CreateMap<AppUser,MyProfileUpdateDto>().ReverseMap();
+
+
+            CreateMap<AppRole,CreateRoleDto>().ReverseMap();
+            CreateMap<AppRole,ListRoleDto>().ReverseMap();
+            CreateMap<AppRole,UpdateRoleDto>().ReverseMap();
         }
     }
 }
